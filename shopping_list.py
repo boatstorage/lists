6b77603ba0dde35498e302c1e139f3a96535a5ca
boatstorage <marinalyulko@gmail.com>
2016-10-25 21:44:41 -0700

    Add files via upload

---
+++ shopping_list.py
@@ -0,0 +1,13 @@
+shopping_list = ["fountain pen", "dice", "le creuset"]
+
+def add_to_list():
+    shopping_list.append.lower()
+    return shopping_list
+
+def order_list():
+    shopping_list.sort()
+    return shopping_list
+
+def not_allowed():
+    if x in shopping_list:
+        print " That already exists in the list."
